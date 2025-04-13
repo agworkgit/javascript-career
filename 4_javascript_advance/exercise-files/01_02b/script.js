24e9f4caf03c6bb4789a384bbe0582628ab5f3e2
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-04-13 16:27:52 +0100

    resumed work on js career

--- 4_javascript_advance/exercise-files/01_02b/script.js
+++ 4_javascript_advance/exercise-files/01_02b/script.js
@@ -1,24 +1,20 @@
 const data = [
-  { id: 1, parent: 0, text: "Top-level comment 1" },
-  { id: 2, parent: 0, text: "Top-level comment 2" },
-  { id: 3, parent: 1, text: "Reply to Top-level comment 1" },
-  { id: 4, parent: 3, text: "Reply to Reply to Top-level comment 1" },
+  { id: 1, parent: 0, text: 'Top-level comment 1' },
+  { id: 2, parent: 0, text: 'Top-level comment 2' },
+  { id: 3, parent: 1, text: 'Reply to top-level comment 1' },
+  { id: 4, parent: 3, text: 'Reply to reply to top-level comment 1' },
 ];
 
 /**
- * Restructure the flat data array into a nested array.
- *
+ * Restructure the flat data array into a nested array
+ * 
  * @param {array} data
- * @returns {array}
- */
+ * @returns {array} 
+*/
+
 function restructureArray(data) {
-  // Create an array to hold the root elements
+  // Create and array to hold the root elements
   const root = [];
 
-  return root;
+
 }
-
-const result = restructureArray(data);
-
-// Output the resut array as a tree.
-console.log(JSON.stringify(result, null, 2));
